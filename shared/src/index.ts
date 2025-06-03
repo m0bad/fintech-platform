72522ec501e39149c3114bc9f6173998e2f074df
Mostafa Badawey <mostafa.badawey@devteam.bg>
2025-06-03 13:38:26 +0300

    Update module exports to include file extensions for types, constants, and utilities

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,8 +1,8 @@
 // Export all types
-export * from './types';
+export * from './types.js';
 
 // Export all constants  
-export * from './constants';
+export * from './constants.js';
 
 // Export all utilities
-export * from './utils'; 
+export * from './utils.js'; 
